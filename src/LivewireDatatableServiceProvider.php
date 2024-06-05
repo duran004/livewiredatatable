ac369b5eac903c240ef6a4099974db75a928ffd8
Duran Can YILMAZ <duran4@rengaver.com>
2024-06-05 04:35:25 +0300

    pusblish views

--- src/LivewireDatatableServiceProvider.php
+++ src/LivewireDatatableServiceProvider.php
@@ -9,7 +9,9 @@ class LivewireDatatableServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'livewire-data-table');
-
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/livewire-data-table'),
+        ], 'livewire-data-table-views');
         \Livewire\Livewire::component('livewire-data-table', LivewireDataTable::class);
     }
 
