c7f5c14c0fb4b2432ecda16c175cb9fc4e555544
Duran Can YILMAZ <duran4@rengaver.com>
2024-07-10 22:38:03 +0300

    model_name added, bugs fixed and FormAjax added with spatie permissions

--- src/LivewireDataTable.php
+++ src/LivewireDataTable.php
@@ -11,24 +11,47 @@ class LivewireDataTable extends Component
 
     public $search = '';
     public $model;
+    public string $model_name = '';
     public string $api_route = '';
     public array $columns = [];
     protected array $protected_columns = ['password', 'remember_token', 'email_verified_at'];
-    public int $perPage = 10;
+    public int $per_page = 10;
 
     protected $queryString = ['search' => ['except' => '']];
 
-    public function mount($model, string $api_route = '')
+    public function mount($model, string $api_route = '', string $model_name = '', int $per_page = 10)
     {
         $this->model = new $model;
         $this->api_route = $api_route;
+        $this->model_name = $model_name;
+        $this->per_page = $per_page;
         $this->columns =  $this->model->getFillable();
         //add id,created_at,updated_at to columns
         array_unshift($this->columns, 'id');
         array_push($this->columns, 'created_at', 'updated_at', 'actions');
         //remove protected columns
         $this->columns = array_diff($this->columns, $this->protected_columns);
+        $this->is_model_have_name();
+        $this->model_name();
     }
+    public function model_name()
+    {
+        // Assign the result of explode() to a variable
+        $exploded = explode('\\', get_class($this->model));
+        // Now pass the variable to end()
+        $this->model_name = end($exploded);
+    }
+
+    public function is_model_have_name()
+    {
+        //varitabanındaki sütünları getir
+        $columns = $this->model->getConnection()->getSchemaBuilder()->getColumnListing($this->model->getTable());
+        //veritabanında name var mı kontrol et varsa id den sonra ekle
+        if (in_array('name', $columns) && !in_array('name', $this->columns)) {
+            $this->columns = array_merge(array_slice($this->columns, 0, 1), ['name'], array_slice($this->columns, 1));
+        }
+    }
+
 
     public function updatingSearch()
     {
@@ -48,8 +71,10 @@ class LivewireDataTable extends Component
                 }
             });
         }
+        //order by id desc
+        $items = $items->orderBy('id', 'desc');
 
-        $items = $items->paginate($this->perPage);
+        $items = $items->paginate($this->per_page);
 
 
         return view('livewire-data-table.livewiredatatable', [
